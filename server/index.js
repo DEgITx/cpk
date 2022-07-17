58e8a947a8657d06ca43e72696f9789dc79ac4ac
Alexey Kasyanchuk <degitx@gmail.com>
2022-07-17 16:08:38 +0300

    increse version

--- server/index.js
+++ server/index.js
@@ -24,6 +24,22 @@ const PACKAGES_DIR = OUT_DIR + 'packages/'
 logT("core", `public path: ${OUT_DIR}`);
 app.use(express.static(OUT_DIR));
 
+function isNumeric(str) {
+    if (typeof str != "string") return false // we only process strings!  
+    return !isNaN(str) && // use type coercion to parse the _entirety_ of the string (`parseFloat` alone does not do this)...
+           !isNaN(parseFloat(str)) // ...and ensure strings of whitespace fail
+  }
+function changeVersion(version) {
+    const versionArr = version.trim().split('.');
+    for(let i = versionArr.length - 1; i > 0; i--) {
+        if (isNumeric(versionArr[i])) {
+            versionArr[i]++;
+            break;
+        }
+    }
+    return versionArr.join('.');
+}
+
 app.post('/publish', async function (req, res) {
     const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress 
 
@@ -49,6 +65,47 @@ app.post('/publish', async function (req, res) {
             return;
         }
 
+        const pkgInfo = await redis.get(`cpk:packages:${package.package}`);
+        if (package.dependencies) {
+            if (!Array.isArray(package.dependencies)) {
+                res.send({
+                    error: true,
+                    errorCode: 3,
+                    errorDesc: `Not array deps`,
+                });
+                return;
+            }
+
+            for (const dep of package.dependencies) {
+                const pkg = await redis.get(`cpk:packages:${dep}`);
+                if (!pkg) {
+                    res.send({
+                        error: true,
+                        errorCode: 4,
+                        errorDesc: `Package from deps not founded ${dep}`,
+                    });
+                    return;
+                }
+            }
+        }
+
+        if (!pkgInfo.version) {
+            package.version = '0.1';
+            logT('version', 'set basic version', package.version);
+        } else {
+            const oldVersion = pkgInfo.version;
+            package.version = changeVersion(pkgInfo.version);
+            if (package.version == oldVersion) {
+                res.send({
+                    error: true,
+                    errorCode: 5,
+                    errorDesc: `Can't change version for ${oldVersion} to ${package.version}`,
+                });
+                return;
+            }
+            logT('version', 'change version from', oldVersion, 'to', package.version);
+        }
+
         logT('zip', 'archive', package);
         if (!fs.existsSync(PACKAGES_DIR + package.package)){
             logT('zip', 'create new dir for package', PACKAGES_DIR + package.package);
@@ -56,8 +113,11 @@ app.post('/publish', async function (req, res) {
         }
         fs.writeFileSync(PACKAGES_DIR + package.package + "/" + 'package.zip', req.body);
         logT('zip', 'archive', package.package, 'saved');
+        fs.writeFileSync(PACKAGES_DIR + package.package + "/" + `package_${package.version}.zip`, req.body);
+        logT('zip', 'archive', `package_${package.version}.zip`, 'saved for version', package.version);
 
         await redis.set(`cpk:packages:${package.package}`, package);
+        await redis.set(`cpk:packages:${package.package}:${package.version}`, package);
     } else {
         logT('package', 'publish', 'ip', ip, 'info', req.body);
         packages[ip] = req.body;
