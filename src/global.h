1d5396e66365a70afdcbe4f5584267f397fa742b
Alexey Kasyanchuk <degitx@gmail.com>
2022-08-09 01:35:57 +0300

    switch to domain

--- src/global.h
+++ src/global.h
@@ -6,11 +6,11 @@ namespace cpk
 {
 
 #ifdef CPK_RELEASE
-#define REMOTE_BACKEND_URL "http://143.244.189.114:9988"
+#define REMOTE_BACKEND_URL "http://cpkpkg.com:9988"
 #define REMOTE_TOOLS_URL REMOTE_BACKEND_URL
 #else
 #define REMOTE_BACKEND_URL "http://127.0.0.1:9988"
-#define REMOTE_TOOLS_URL "http://143.244.189.114:9988"
+#define REMOTE_TOOLS_URL "http://cpkpkg.com:9988"
 #endif
 
 size_t FileSize(const std::string& filename);
